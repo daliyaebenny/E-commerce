c04d88ec3d698eb54145d5a5584dd55c8501f0ac
Daliya Benny <daliyaebenny@gmail.com>
2021-12-31 01:21:15 -0800

    Code change for product's GET route &Categories UPDATE route

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -34,7 +34,7 @@ Product.init(
     category_id:{
       type: DataTypes.INTEGER,
       references: {
-        model: 'categry',
+        model: 'category',
         key: 'id',
         unique: false
       }
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -17,14 +17,13 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'tag_products' }]
-    });
-
+    
+    const productData = await Product.findByPk(req.params.id);
+    
     if (!productData) {
       res.status(404).json({ message: 'No traveller found with this id!' });
       return;
@@ -38,7 +37,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -70,7 +69,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -111,7 +110,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete one product by its `id` value
 
   try {
